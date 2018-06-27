954e6a5b84293a2c8fc529314bac144678e40b21
macong <1103849875@qq.com>
2018-06-27 10:09:00 +0800

    test

--- application/api5/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//配置文件
-return [
-    // 扩展函数文件
-    'extra_file_list'        => [ APP_PATH . 'common/helper.php', THINK_PATH . 'helper.php', THINK_PATH . 'extra_helper' . EXT,],
-    // 显示错误信息
-    'show_error_msg'         => true,
-    'exception_handle'       => '\app\common\exception\Http',
-];
--- runtime2/cache/data/user_login_4_.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000002592000a:3:{s:9:"nick_name";s:15:"平台马甲号";s:10:"login_name";s:11:"18310316264";s:8:"user_key";s:60:"$2y$10$6DKxwrvdq9M63z3kpXy.nuwv/kmkyunf1SOfgbD65eJGjljiK45Ci";}
-?>
